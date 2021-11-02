e3d125dde10b29d10119957ff345190bdab7b839
MarcaCriss <54193589+MarcaCriss@users.noreply.github.com>
2021-11-02 10:57:22 -0400

    configured and execute migrations

---
+++ src/database/migrations/1635864451337-init.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class init1635864451337 implements MigrationInterface {
+  name = 'init1635864451337';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE \`product\` (\`id\` int NOT NULL AUTO_INCREMENT, \`name\` varchar(255) NOT NULL, \`description\` text NOT NULL, \`price\` int NOT NULL, \`stock\` int NOT NULL, \`image\` varchar(255) NOT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE \`product\``);
+  }
+}
